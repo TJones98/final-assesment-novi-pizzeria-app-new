8a5be1a0540117a8d1be5884797c5b8455cb1b89
TJones98 <mailtessjones@gmail.com>
2026-02-01 18:35:22 +0100

    built base version staff dashboard and adjusted navbar

--- src/pages/StaffDashboard/StaffDashboard.jsx
+++ src/pages/StaffDashboard/StaffDashboard.jsx
@@ -1,7 +1,24 @@
+import Card from '../../components/Card/Card.jsx';
+import PageTitle from '../../components/PageTitle/PageTitle.jsx';
+import Button from '../../components/Button/Button.jsx';
+import './StaffDashboard.css'
+import {useContext} from "react";
+import {AuthContext} from "../../contexts/AuthContext.jsx";
+
 function StaffDashboard() {
+    const {userData} = useContext(AuthContext);
+
     return (
         <>
-            <h1>Staff Page</h1>
+            <PageTitle title="Personeelsdashboard" subtitle="Werk ze vandaag!"/>
+            <div className="dashboard-buttons">
+                <Button buttonType="button" buttonText="Openstaande bestellingen" onClick="onClick"/>
+                <Button buttonType="button" buttonText="Afgeronde bestellingen" onClick="onClick"/>
+                <Button buttonType="button" buttonText="Bewerk menu" disabled={!userData.roles.includes("admin")} onClick="onClick"/>
+            </div>
+            <Card>
+                <p>card content</p>
+            </Card>
         </>
 )}
 
